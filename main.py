87b5fba1cb69a42af086610cf9cd79c0a7086d11
rahul-mallah <mallahrahul@gmail.com>
2025-01-23 17:23:07 +0800

    Add 3 lives for the player

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
+from live import Live
 
 def main():
     pygame.init()
@@ -18,15 +19,25 @@ def main():
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     bullets = pygame.sprite.Group()
+    lives = pygame.sprite.Group()
     Shot.containers = (bullets, updatable, drawable)
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
+
     
     AsteroidField.containers = updatable
-    
+    Live.containers = (lives, updatable, drawable)
+
     asteroid_field = AsteroidField() 
     player  = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+    positionX = SCREEN_WIDTH/30
+    positionY = SCREEN_HEIGHT/16
+    
+    lives = []
+    for i in range(0, 3):
+        lives.append(Live(positionX, positionY))
+        positionX += 50
 
     while True:
         for event in pygame.event.get():
@@ -38,8 +49,13 @@ def main():
         
         for obj in asteroids:
             if obj.collided(player):
-                print("Game over!")
-                return
+                if len(lives) > 0:
+                    obj.kill()
+                    live = lives.pop()
+                    live.kill()
+                else:
+                    print("Game over!")
+                    return
             for bullet in bullets:
                 if bullet.collided(obj):
                     obj.split()
